2e3d80b09f29705e5b6cb5fec43c3d9b7c07f68c
Cameron Brown <cameronjoebrown@gmail.com>
2021-02-13 18:41:59 -0700

    Updated README.md and generated components for Home, Projects, Contact, and About Me pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ProjectsComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
