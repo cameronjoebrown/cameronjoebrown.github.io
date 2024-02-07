982d98a87b4ba4e4840ca4e8871c260dc297900b
Cameron Brown <cameronjoebrown@gmail.com>
2024-02-06 19:40:39 -0700

    Update style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'portfolio', component: PortfolioComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'about', component: AboutMeComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -15,7 +14,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
         AppComponent,
         HomeComponent,
         ContactComponent,
-        PortfolioComponent,
         AboutMeComponent
     ],
     imports: [BrowserModule, AppRoutingModule, NgbModule, FontAwesomeModule],
