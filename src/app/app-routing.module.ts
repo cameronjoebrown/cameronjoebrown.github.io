7bf352513e12080e72c85acc6d5752bfe2aeaba0
Cameron Brown <cameronjoebrown@gmail.com>
2024-04-07 13:07:31 -0600

    Add routing to projects page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'about', component: AboutMeComponent }
+    { path: 'about', component: AboutMeComponent },
+    { path: 'projects', component: ProjectsComponent },
 ];
 
 @NgModule({
