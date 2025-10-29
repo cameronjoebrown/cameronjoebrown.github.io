b9acb797f44ee4b88e9e2b6a91a9c23dc18ba4bf
Cameron Brown <cameronjoebrown@gmail.com>
2025-10-28 18:16:33 -0600

    Update Project Page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  openSourceProjects = [
+    {
+      title: 'Portfolio Website',
+      description: 'This website is a showcase of my open source work and professional experience',
+      technologies: ['Angular', 'Bootstrap'],
+      gitHubUrl: 'https://github.com/cameronjoebrown/portfolio-website',
+      liveUrl: 'https://cameronjoebrown.com'
+    }
+  ];
 
+  workProjects = [
+    {
+      title: 'Enterprise Financial Platform',
+      description: "A platform for managing and analyzing enterprise financial data",
+      technologies: ['React', 'Spring Boot', 'AWS'],
+      company: 'Pivotal',
+      year: '2023'
+    }
+  ]
+
+  ngOnInit(): void {
+      
+  }
 }
