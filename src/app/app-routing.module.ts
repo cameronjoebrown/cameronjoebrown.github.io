01d1b5df382795adfc350b6e223139771a688857
Cameron Brown <cameronjoebrown@gmail.com>
2026-03-03 16:28:41 -0700

    Remove contact component and add polyfills back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'contact', component: ContactComponent },
     { path: 'about', component: AboutMeComponent },
     { path: '**', redirectTo: '' }
 ];
