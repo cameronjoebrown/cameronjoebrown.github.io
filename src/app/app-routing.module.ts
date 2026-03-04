354da215e6318956166d5371931ed1d0557739eb
Cameron Brown <cameronjoebrown@gmail.com>
2026-03-03 18:05:22 -0700

    Add projects page (#93)
    * Add angular schematics that was missing
    
    * Generate initial projects component
    
    * Add routing to projects page
    
    * Get title aligned correctly
    
    * Start adding projects
    
    * Update Project Page
    
    * update yarn.lock
    
    * slight updates
    
    * fix linting issues
    
    * replace ngfor with for
    
    * get github url working
    
    * Add another project and make liveUrl conditional
    
    * add another project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutMeComponent },
+    { path: 'projects', component: ProjectsComponent },,
     { path: '**', redirectTo: '' }
 ];
 
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent implements OnInit {
+  openSourceProjects = [
+    {
+      title: 'Portfolio Website',
+      description: 'The code for this portfolio website, built with Angular and Bootstrap. It is currently deployed using a Github Action and hosted on GitHub Pages.',
+      technologies: ['Angular', 'Bootstrap', 'TypeScript', 'GitHub Actions', 'GitHub Pages'],
+      gitHubUrl: 'https://github.com/cameronjoebrown/cameronjoebrown.github.io',
+      liveUrl: 'https://cameronjoebrown.com'
+    },
+    {
+      title: 'Iron Blossom Web Scraper',
+      description: 'A web scraper of the Iron Blossom Lodge Timeshares for Sale website that sends an email when a new Timeshare unit is posted for sale.',
+      technologies: ['Python', 'BeautifulSoup'],
+      gitHubUrl: 'https://github.com/cameronjoebrown/ironblossom-web-scraper',
+    }
+  ];
+
+  workProjects = [
+    {
+      title: 'Digital Automation Platform UI',
+      description: "A web-based UI for managing and visualizing BPMN workflows in Camunda 7.",
+      technologies: ['Angular', 'Cypress', 'Playwright', 'Kubernetes', 'AWS', 'TypeScript'],
+      company: 'Fidelity Investments',
+      year: '2022-2024'
+    },
+    {
+      title: 'Enterprise Task Management API',
+      description: 'A REST API that provides additional capabilities on top of the existing task resources in the Camunda 7 platform.',
+      technologies: ['Node.js', 'NestJS', 'Kubernetes', 'PostgreSQL', 'Camunda 7', 'AWS', 'TypeScript'],
+      company: 'Fidelity Investments',
+      year: '2024-Present'
+    }
+  ]
+
+  ngOnInit(): void {
+      
+  }
+}
